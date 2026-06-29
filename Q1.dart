b7889eeb24b9653c2033e21aaa4d5e363cca0694
Muhammad Afzal <vafzal078@gmail.com>
2026-06-29 17:42:38 +0500

    Update Q1.dart
    Restored the original program structure and comments.

--- Q1.dart
+++ Q1.dart
@@ -1,3 +1,5 @@
+// 1). Create a program that asks the user to enter their name and their age. Print out a message that tells how many years they have to be 100 years old.
+
 import 'dart:io';
 
 void main() {
