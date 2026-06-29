b1ee5118e6eec3940c7618ceb4b44dd879f8fd43
Muhammad Afzal <vafzal078@gmail.com>
2026-06-29 16:55:00 +0500

    Add files via upload

---
+++ Q1.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+  print("enter your name:");
+  String name = stdin.readLineSync() ?? "";
+
+  print("enter your age:");
+  int age = int.parse(stdin.readLineSync() ?? "0");
+
+  int yearsLeft = 100 - age;
+  print("$name you have $yearsLeft years until you turn 100.");
+}
+
+/* 
+
+output: 
+
+enter your name:
+afzal
+enter your age:
+23
+afzal you have 77 years until you turn 100.
+
+*/
